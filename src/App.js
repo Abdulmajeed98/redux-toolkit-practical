da4e282f16cafa5e81496fdad419fd7da83278c7
Abdulmajeed Jaafer <abdulmajeedjaafer18@gmail.com>
2022-02-25 18:55:33 +0300

    add: pages and routes to the App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Counter from "./pages/Counter";
+import Employees from "./pages/Employees";
+import Home from "./pages/Home";
+import UserList from "./pages/UserList";
 function App() {
-  return <div className="container">App</div>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/employees" element={<Employees />} />
+        <Route path="/users" element={<UserList />} />
+        <Route path="/counter" element={<Counter />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
